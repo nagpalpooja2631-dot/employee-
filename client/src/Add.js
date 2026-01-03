456f3e35eed5c1b5c1fa5afc4f890723563bf604
nagpalpooja2631-dot <nagpalpooja2631@gmail.com>
2026-01-04 00:19:16 +0530

    Update API endpoint for employee submission

--- client/src/Add.js
+++ client/src/Add.js
@@ -13,7 +13,7 @@ function Add(){
                 e.preventDefault();
                 try
                 {
-                    const response= await axios.post('http://localhost:3001/api/employess',{empNo,empName,empSal});
+                    const response= await axios.post('https://employee-k9t6.onrender.com',{empNo,empName,empSal});
                     //alert(JSON.stringify(response.data,null,2));
                     alert(response.data.message);
 
